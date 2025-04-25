7f83ffeaab158bcd386c40ff62284af85c652bbe
Leonam <lpdmonteiro@gmail.com>
2025-04-24 22:23:52 -0300

    novos exercicios (for e while)

---
+++ modulo-01-introducao/atividades/loops.js
@@ -0,0 +1,9 @@
+//Exercício 1
+let numero = 0;
+let multiplicador = 3;
+let resultado = 0;
+while (numero < 10) {
+  numero++;
+  resultado = numero * multiplicador;
+  console.log(`${numero} * ${multiplicador} = ${resultado}`);
+}
