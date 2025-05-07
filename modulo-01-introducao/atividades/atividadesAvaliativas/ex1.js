7ba0b9e1d7e2bce353cbf392cef90b18f9fb0fa5
Leonam <lpdmonteiro@gmail.com>
2025-05-07 20:10:34 -0300

    Adicionado função mostrarMenu

--- modulo-01-introducao/atividades/atividadesAvaliativas/ex1.js
+++ modulo-01-introducao/atividades/atividadesAvaliativas/ex1.js
@@ -1,4 +1,5 @@
-// exercicios/ex1.js
+// 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
+// utilizando uma estrutura de controle if.
 const prompt = require("prompt-sync")({ sigint: true });
 
 function ex1() {
